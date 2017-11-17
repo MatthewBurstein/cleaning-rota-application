6a47570ebb57fcf545a608b0bb86d1e22e55b595
Matt Burstein <j.matthew.burstein@gmail.com>
2017-11-17 17:38:08 +0000

    creating directories and rotas using test directory maker works but could be  refactored to replace strings in formulae with variables?

--- lib/test_directory_maker.rb
+++ lib/test_directory_maker.rb
@@ -1,6 +1,19 @@
+test_rota_var = "test_rota_name"
+parent_directory = "/Users/matt/Coding/Cleaning Rota App"
 
-  Dir.chdir("../rotas/") { |directory| Dir.mkdir("test_rota_name")}
+if !File.exist?("#{parent_directory}/rotas")
+  Dir.chdir("#{parent_directory}") { |directory| Dir.mkdir("rotas")}
+end
 
-  Dir.chdir("../rotas/test_rota_name")
+rotas_directory = "#{parent_directory}/rotas"
 
-  File.new("testfile.txt", "w+")
+if !File.exist?("#{rotas_directory}/#{test_rota_var}")
+  Dir.chdir("#{rotas_directory}") { |directory| Dir.mkdir("#{test_rota_var}")}
+end
+
+Dir.chdir("#{rotas_directory}/#{test_rota_var}")
+
+File.new("testfile.txt", "w+")
+
+
+#Dir.chdir("../rotas/") { |directory| Dir.mkdir("test_rota_name")}
