279b847ff25de84fdd6aeca918cbe93502b890c8
Matt Burstein <j.matthew.burstein@gmail.com>
2017-11-19 20:41:28 +0000

    started view_rota_wizard. open_rota_csv largely complete. working on converting housemates to same format as create rota wizard to make it easier to work with them

--- lib/view_rota_wizard.rb
+++ lib/view_rota_wizard.rb
@@ -1,9 +1,23 @@
+require "CSV"
+
 class ViewRotaWizard
   attr_accessor :rota
+  attr_reader :directory
 
   def initialize(rota)
     @rota = rota
+    @directory = "/Users/matt/Coding/Cleaning Rota App/rotas"
     puts "what would you like to do?"
+  end
+
+  def open_rota_csv
+    housemates = CSV.read("#{@directory}/#{@rota}/#{@rota}_rota.csv", return_headers:false)
+    rota_headers = housemates.shift
+    puts "housemates is #{housemates}"
+    puts "rota_headers is #{rota_headers}"
+  end
+
+  def open_rooms_csv
   end
 
   def what_to_do
@@ -25,6 +39,11 @@ class ViewRotaWizard
   end
 
 end
+
+rota = ViewRotaWizard.new("MyRota")
+rota.open_rota_csv
+
+
 
 =begin
 general_rotas_directory = "/Users/matt/Coding/Cleaning Rota App/rotas"
